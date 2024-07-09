0a13d599404fe5e8036c892d9e5e18315f0fff86
Unknown <unknown@example.com>
2024-07-09 15:23:27 +0800

    新增对点云时间同步和去畸变部分的阅读

--- src/cartographer/cartographer/mapping/internal/2d/local_trajectory_builder_2d.cc
+++ src/cartographer/cartographer/mapping/internal/2d/local_trajectory_builder_2d.cc
@@ -101,10 +101,21 @@ std::unique_ptr<transform::Rigid2d> LocalTrajectoryBuilder2D::ScanMatch(
   return pose_observation;
 }
 
+/**
+ * @brief 处理点云数据, 进行扫描匹配, 将点云写成地图
+ * 
+ * @param[in] sensor_id 点云数据对应的话题名称
+ * @param[in] unsynchronized_data 传入的点云数据
+ * @return std::unique_ptr<LocalTrajectoryBuilder2D::MatchingResult> 匹配后的结果
+ */
 std::unique_ptr<LocalTrajectoryBuilder2D::MatchingResult>
 LocalTrajectoryBuilder2D::AddRangeData(
     const std::string& sensor_id,
-    const sensor::TimedPointCloudData& unsynchronized_data) {
+    const sensor::TimedPointCloudData& unsynchronized_data) 
+{
+
+  // Step: 1 进行多个雷达点云数据的时间同步, 点云的坐标是相对于tracking_frame的
+  // 调用range_data_collator_的AddRangeData方法来同步激光雷达数据
   auto synchronized_data =
       range_data_collator_.AddRangeData(sensor_id, unsynchronized_data);
   if (synchronized_data.ranges.empty()) {
@@ -112,48 +123,74 @@ LocalTrajectoryBuilder2D::AddRangeData(
     return nullptr;
   }
 
+  // 从同步数据中获取当前时间戳
   const common::Time& time = synchronized_data.time;
   // Initialize extrapolator now if we do not ever use an IMU.
-  if (!options_.use_imu_data()) {
+  // 如果不用imu, 就在雷达这初始化位姿推测器
+  if (!options_.use_imu_data()) 
+  {
     InitializeExtrapolator(time);
   }
-
-  if (extrapolator_ == nullptr) {
+  // 说明位姿估计器还没初始化好
+  if (extrapolator_ == nullptr) 
+  {
     // Until we've initialized the extrapolator with our first IMU message, we
     // cannot compute the orientation of the rangefinder.
     LOG(INFO) << "Extrapolator not yet initialized.";
     return nullptr;
   }
 
+  // 确保同步数据的范围不为空，并且最后一个点的时间戳小于等于0
   CHECK(!synchronized_data.ranges.empty());
   // TODO(gaschler): Check if this can strictly be 0.
   CHECK_LE(synchronized_data.ranges.back().point_time.time, 0.f);
+
+  // 计算第一个点的实际时间，并检查该时间是否早于姿态预测器的最后一个姿态时间。如果是，记录日志并返回 nullptr。
   const common::Time time_first_point =
       time +
       common::FromSeconds(synchronized_data.ranges.front().point_time.time);
+  // 只有在extrapolator_初始化时, GetLastPoseTime()是common::Time::min()
   if (time_first_point < extrapolator_->GetLastPoseTime()) {
     LOG(INFO) << "Extrapolator is still initializing.";
     return nullptr;
   }
 
+  // 初始化姿态向量来存储每个范围数据点时刻的机器人位姿，并预留空间以避免频繁的内存分配
   std::vector<transform::Rigid3f> range_data_poses;
   range_data_poses.reserve(synchronized_data.ranges.size());
+
   bool warned = false;
-  for (const auto& range : synchronized_data.ranges) {
+
+  // 遍历同步数据中的每个范围数据点
+  for (const auto& range : synchronized_data.ranges) 
+  {
+    // 根据同步数据的时间 time 和范围数据点的相对时间 range.point_time.time 计算每个范围数据点的实际时间点 time_point
     common::Time time_point = time + common::FromSeconds(range.point_time.time);
-    if (time_point < extrapolator_->GetLastExtrapolatedTime()) {
-      if (!warned) {
+
+    // 检查计算出的时间点 time_point 是否早于姿态预测器最后一次外推的时间
+    // 如果该时间比上次预测位姿的时间还要早,说明这个点的时间戳往回走了, 就报错
+    if (time_point < extrapolator_->GetLastExtrapolatedTime()) 
+    {
+      // 如果是，并且尚未发出警告，一个循环只报一次错
+      if (!warned) 
+      {
+        // 记录一条错误信息并将 warned 标志设为 true。
         LOG(ERROR)
             << "Timestamp of individual range data point jumps backwards from "
             << extrapolator_->GetLastExtrapolatedTime() << " to " << time_point;
         warned = true;
       }
+      // 同时，将时间点重置为姿态预测器最后一次外推的时间，以确保时间点不向后跳跃
       time_point = extrapolator_->GetLastExtrapolatedTime();
     }
+    // 使用姿态预测器根据时间点 time_point 计算出该时间点的姿态
+    // 使用修正后的时间点来进行姿态外推
+    // Step: 2 预测出 每个点的时间戳时刻, tracking frame 在 local slam 坐标系下的位姿
     range_data_poses.push_back(
         extrapolator_->ExtrapolatePose(time_point).cast<float>());
   }
 
+  // 当 num_accumulated_ 为 0 时，表示这是第一个要累积的点云数据。因此，需要初始化 accumulated_range_data_ 以便开始累积新的数据
   if (num_accumulated_ == 0) {
     // 'accumulated_range_data_.origin' is uninitialized until the last
     // accumulation.
@@ -162,27 +199,48 @@ LocalTrajectoryBuilder2D::AddRangeData(
 
   // Drop any returns below the minimum range and convert returns beyond the
   // maximum range into misses.
-  for (size_t i = 0; i < synchronized_data.ranges.size(); ++i) {
-    const sensor::TimedRangefinderPoint& hit =
-        synchronized_data.ranges[i].point_time;
+  // 遍历所有同步的范围数据
+  for (size_t i = 0; i < synchronized_data.ranges.size(); ++i) 
+  {
+    // 获取在tracking frame 下带时间戳的单个数据点的坐标 xyz
+    const sensor::TimedRangefinderPoint& hit = synchronized_data.ranges[i].point_time;
+    // / 将点云的origins坐标系原点转到 local slam 坐标系下
     const Eigen::Vector3f origin_in_local =
+        // 当前点时刻下机器人在本地坐标系中的位姿
         range_data_poses[i] *
+        // 获取该点的坐标系原点
         synchronized_data.origins.at(synchronized_data.ranges[i].origin_index);
+
+    // 计算当前点与传感器原点的距离
+    // Step: 3 运动畸变的去除, 将相对于tracking_frame的hit坐标 转成 local坐标系下的坐标
+    // 点云数据从传感器坐标系转换到本地SLAM坐标系
     sensor::RangefinderPoint hit_in_local =
         range_data_poses[i] * sensor::ToRangefinderPoint(hit);
+
+    // 在local slam 坐标系下用点云的位置减去传感器坐标系的位置，得到激光点和原点之间的向量
     const Eigen::Vector3f delta = hit_in_local.position - origin_in_local;
+    // 计算激光点的距离
     const float range = delta.norm();
-    if (range >= options_.min_range()) {
-      if (range <= options_.max_range()) {
+    // 检查距离是否在有效范围内
+    if (range >= options_.min_range()) 
+    {
+      if (range <= options_.max_range()) 
+      {
+        // 将距离在最大有效范围内的点分类为有效返回
         accumulated_range_data_.returns.push_back(hit_in_local);
-      } else {
+      } 
+      else 
+      {
+        // 如果距离大于最大有效距离 max_range，则将激光点的位置调整到一个固定的缺失数据长度 missing_data_ray_length
         hit_in_local.position =
             origin_in_local +
             options_.missing_data_ray_length() / range * delta;
+        // 并添加到 accumulated_range_data_ 的 misses（缺失数据）中。
         accumulated_range_data_.misses.push_back(hit_in_local);
       }
     }
   }
+  // 获得了一帧有效数据
   ++num_accumulated_;
 
   if (num_accumulated_ >= options_.num_accumulated_range_data()) {
